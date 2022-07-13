4fbac3e8e7e9da99eb3402bac4a96e46f33eea62
Nayab576 <98881837+Nayab576@users.noreply.github.com>
2022-07-13 11:43:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,15 @@
 variable "region_name" {
- #default = "us-east-1"
+ default = "us-east-1"
 }
 variable "instance_type" {
  description = "instance type for ec2 instance"
- #default = "t2.micro"
+ default = "t2.micro"
 }
 
 variable "ami_id" {
   description = "AMI ID"
- # default = "ami-04505e74c0741db8d"
+ default = "ami-052efd3df9dad4825"
 }
 variable "key_name"{
+ default = "cjptech"
 }
